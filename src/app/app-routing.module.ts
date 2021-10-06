12a59b3d7e798614852c0fd7a656246a02fddc5e
Jerferson <jerfersongriza@gmail.com>
2021-10-05 23:27:35 -0300

    feat(business): add business and card modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessModule } from './modules/business/business.module';
+import { CardModule } from './modules/card/card.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ():Promise<BusinessModule> => import('./modules/business/business.module')
+      .then(m => m.BusinessModule)
+  },
+  {
+    path: 'card',
+    loadChildren: (): Promise<CardModule> => import('./modules/card/card.module')
+      .then(m => m.CardModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
