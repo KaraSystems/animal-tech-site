a10a78e166fefafb28e66022ee521109bc38f602
Jerferson <jerfersongriza@gmail.com>
2021-10-08 14:09:44 -0300

    feat(login): add login module

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'app/modules/login/services/login.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public loginService: LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  openLogin(): void {
+    this.loginService.openLogin();
   }
 
 }
